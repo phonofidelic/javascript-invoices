2a09e0f13175354b37ba37fb9dac081021fa7728
Christopher Clemons <chris.clemons85@gmail.com>
2016-11-11 15:06:18 -0500

    feat: add basic functionality for invoices
    Button for creating a new invoice adds a new invoice to db.
    Individual invoice items customer id can be specified and
    persists in db. Products are selectable from each invoice item.
    
    Bug: product items are set outside of invoice ites scope so they
    are added to every invoice.

--- public/js/modules/Invoices/Invoices.js
+++ public/js/modules/Invoices/Invoices.js
@@ -1,18 +1,96 @@
 'use-strict';
 
 angular.module('invoiceApp').controller('InvoicesController', ['$scope', '$http', '$log', 'InvoicesService', function($scope, $http, $log, InvoicesService) {
-	// $scope.invoices = [];
+	$scope.invoices = [];
+	$scope.selectedProducts = [];
 
-	$http.get('api/invoices').then(function(response) {
-		$log.log('getInvoices response:', response);
-		$scope.invoices = response.data;
 
-		$log.log($scope.invoices)
-	});
+	$scope.getInvoices = function() {
+		$http.get('/api/invoices').then(function(response) {
+			$log.log('getInvoices response:', response);
+			$scope.invoices = response.data;
+		});
+	};
 
-	$scope.createNewInvoice = function() {
-		$log.log('create new invoice')
+	$scope.addInvoice = function() {
+		$log.log('create new invoice');
+		$scope.invoices.push({});
+		$http.post('/api/invoices', {}).then(function(response) {
+			$log.log('invoice put response:', response);
+			$http.post('/api/invoices/' + response.data.id + '/items', {invoiceItems: []}).then(function(response) {
+				$log.log('addInvoiceItems response:', response);
+			});
+			$scope.getInvoices();
+		});
+	};
+
+	$scope.deleteInvoice = function(id, index) {
+		$http.delete('api/invoices/' + id).then(function(response) {
+			$log.log('invoice delete response:', response);
+			$scope.invoices.splice(index, 1);
+		});
+	};
+
+	$scope.addInvoiceItems = function(id) {
+		$http.put('/api/invoices' + id + '/items').then(function(response) {
+			$log.log('addInvoiceItems response:', response);
+		});
+	};
+
+	$scope.getInvoiceItems = function(id) {
+		$http.get('/api/invoices/' + id + '/items').then(function(response) {
+			$log.log('getInvoiceItems response:', response);
+
+		});
+	};
+
+	$scope.getCustomers = function() {
+		$http.get('/api/customers').then(function(response) {
+			$log.log('customers response:', response, '*****');
+			$scope.customers = response.data;
+		});
+	};
+
+	$scope.selectCustomer = function(invoice, customer) {
+		// $log.log('customer:', customer)
+		// var parsedCustomer = JSON.parse(customer);
+		var parsedCustomer = customer;
+		$log.log('parsedCustomer:', parsedCustomer.id)
+		$http.put('/api/invoices/' + invoice.id, { customer_id: parsedCustomer.id }).then(function(response) {
+			$log.log('selectCustomer put response:', response);
+			$scope.getInvoices();
+		}).catch(function(err) {
+			$log.error('selectCustomer error:', err);
+		});
+	};
+
+	$scope.getProducts = function() {
+		$http.get('/api/products').then(function(response) {
+			$log.log('products response:', response);
+			$scope.products = response.data;
+		});
+	};
+
+	$scope.addProduct = function(item, invoiceId) {
+		if (angular.isDefined(item)) {
+			var parsedItem = JSON.parse(item);
+			$scope.selectedProducts.push(parsedItem);
+			$log.log('addProduct:', parsedItem)
+			$http.put('/api/invoices/' + invoiceId + '/items/' + parsedItem.id, parsedItem).then(function(response) {
+				$log.log('product put response');
+				// $scope.selectedProducts.push(parsedItem);
+				$log.log('selectedProducts:', $scope.selectedProducts);
+			}).catch(function(err) {
+				$log.error('addProduct error:', err);
+			});			
+		};
+	};
+
+	$scope.deleteProduct = function(index) {
+		$scope.selectedProducts.splice(index, 1);
 	}
+
+	// getInvoices();
 }]);
 
 angular.module('invoiceApp').factory('InvoicesService', ['$http', '$log', function($http, $log) {
